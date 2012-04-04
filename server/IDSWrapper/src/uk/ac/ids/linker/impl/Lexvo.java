e3cbfa416a6de8ce7c3b45583172c5f0f27c3ad2
Christophe Gueret <christophe.gueret@gmail.com>
2012-04-04 11:41:24 +0200

    Improved Lexvo linker

--- server/IDSWrapper/src/uk/ac/ids/linker/impl/Lexvo.java
+++ server/IDSWrapper/src/uk/ac/ids/linker/impl/Lexvo.java
@@ -6,10 +6,14 @@ package uk.ac.ids.linker.impl;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.logging.Logger;
 
 import org.restlet.data.Reference;
+import org.restlet.ext.rdf.Graph;
+import org.restlet.ext.rdf.GraphBuilder;
+import org.restlet.ext.rdf.Link;
+import org.restlet.ext.rdf.internal.xml.RdfXmlReader;
+import org.restlet.representation.StringRepresentation;
 
 import uk.ac.ids.linker.Linker;
 import uk.ac.ids.linker.LinkerParameters;
@@ -39,15 +43,15 @@ public class Lexvo extends Linker {
 	@Override
 	protected Reference getFromService(LinkerParameters parameters) {
 		// Get the main page of the language
-		String result = LexvoIdentifiersAPI.getTermURI(parameters.get(LANG_NAME), parameters.get(LANG_NAME_LOCALE));
+		String targetStr = LexvoIdentifiersAPI.getTermURI(parameters.get(LANG_NAME), parameters.get(LANG_NAME_LOCALE));
+		Reference target = new Reference(targetStr);
 
 		// Look for the reference to the iso639-3 resource
 		try {
 
 			// Load the RDF
 			StringBuffer response = new StringBuffer();
-			URL url = new URL(result);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			HttpURLConnection connection = (HttpURLConnection) target.toUrl().openConnection();
 			connection.setRequestProperty("Accept", "application/rdf+xml");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String line;
@@ -55,26 +59,30 @@ public class Lexvo extends Linker {
 				response.append(line);
 			}
 			reader.close();
+			Graph graph = new Graph();
+			GraphBuilder builder = new GraphBuilder(graph);
+			RdfXmlReader r = new RdfXmlReader(new StringRepresentation(response), builder);
+			r.parse();
 
-			// FIXME the RDF from Lexvo is broken at the moment
-			// Graph graph = new Graph();
-			// GraphBuilder builder = new GraphBuilder(graph);
-			// RdfXmlReader r = new RdfXmlReader(new
-			// StringRepresentation(response), builder);
-			// r.parse();
+			// Look for the desired value
+			for (Link stmt : graph)
+				if (stmt.getSourceAsReference().equals(new Reference(target)))
+					if (stmt.getTypeRef().equals(new Reference("http://lexvo.org/ontology#means")))
+						if (stmt.getTargetAsReference().toString().startsWith(ISO639_3_PREFIX))
+							target = stmt.getTargetAsReference();
+
 		} catch (Exception e) {
 		}
 
-		return new Reference(result);
+		return target;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		LinkerParameters parameters = new LinkerParameters();
-		parameters.put(Lexvo.LANG_NAME, "English");
+		parameters.put(Lexvo.LANG_NAME, "Spanish");
 		parameters.put(Lexvo.LANG_NAME_LOCALE, "eng");
 		Lexvo l = new Lexvo();
 		logger.info(l.getFromService(parameters).toString());
