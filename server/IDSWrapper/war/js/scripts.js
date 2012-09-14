00d536e984980e83478bcbad09ba6719f8938820
Christophe Gueret <christophe.gueret@gmail.com>
2012-09-14 23:10:55 +0200

    Report on the number of results found

--- server/IDSWrapper/war/js/scripts.js
+++ server/IDSWrapper/war/js/scripts.js
@@ -1,12 +1,31 @@
 function doSearch() {
 	$("#search_results").empty();
 	$.getJSON('./eldis-ids/search/' + $("#searchTerm").val(), function(data) {
-		$.each(data.results, function(index, item) {
-			var link = $("<a>").attr("href",window.location + "/" + item.url).text(item.label);
-			var entry = $("<li>");
-			entry.append(link);
-			$("#search_results").append(entry);
-		});
+		if (data.results.length > 0) {
+			var message = $("<div>").attr("class","alert alert-success").text(data.results.length + " results !");
+			$("#search_results").append(message);
+			
+			var list = $("<ul>");
+			console.log(data.results.length);
+			$.each(data.results, function(index, item) {
+				var link = $("<a>").attr("href",
+						window.location + "/" + item.url).text(item.label);
+				var entry = $("<li>");
+				entry.append(link);
+				list.append(entry);
+			});
+			$("#search_results").append(list);
+		} else {
+			var message = $("<div>").attr("class","alert alert-error").text("No results :-(");
+			$("#search_results").append(message);
+			
+		}
 	});
 	return false;
+}
+
+function update(event) {
+	if (event.keyCode == 13) {
+		doSearch();
+	}
 }
